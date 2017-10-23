a8ccdc24947341e7250ea1ed49b287467e83ccce
David Bradway <dpb6@duke.edu>
2017-10-23 14:06:23 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import numbers
+app = Flask(__name__)
+
+
+@app.route("/api/hello/<name>", methods=['GET'])
+def hello(name):
+    return "Hello World! " + name
+
+
+@app.route("/api/data", methods=['GET'])
+def data():
+    my_dict = {
+               "temp": [20, 21, 21],
+               "time": [10, 20, 30],
+               "unit": "s"
+              }
+    return jsonify(my_dict)
+
+
+@app.route("/api/add", methods=['POST'])
+def add():
+    if (request.is_json):
+        content = request.get_json()
+        if 'a' in content and 'b' in content:
+            if isinstance(content['a'], numbers.Number) and isinstance(content['b'], numbers.Number):
+                return str(content['a'] + content['b']), 200
+    return {}, 400
